facdaecbd89709af3a84b4998fdd155ec3d1f626
nkichenkov <151444715+nkichenkov@users.noreply.github.com>
2023-11-23 15:30:12 +0300

    .

--- src/lesson13/exercise05/Solution.java
+++ src/lesson13/exercise05/Solution.java
@@ -6,4 +6,33 @@ package lesson13.exercise05;
  */
 
 public class Solution {
+    public static void main(String[] args) {
+        print(emptyStringCount(new String[]{"a", "", "sd", ""}));
+        print(emptyStringCount(new String[]{""}));
+        print(emptyStringCount(new String[]{" "}));
+        print(emptyStringCount(new String[]{}));
+    }
+
+    private static int emptyStringCount(String[] array) {
+        int count = 0;
+        if (array.length < 1) {
+            print("array is empty");
+            count = -1;
+        } else {
+            for (String string : array) {
+                if (string.isEmpty()) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private static void print(int x) {
+        System.out.println(x);
+    }
+
+    private static void print(String s) {
+        System.out.println(s);
+    }
 }
