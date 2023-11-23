3cabc73a06e51c1a286810410c8f665d980fd16c
nkichenkov <151444715+nkichenkov@users.noreply.github.com>
2023-11-23 18:23:07 +0300

    .

--- src/lesson14/exercise01/Solution.java
+++ src/lesson14/exercise01/Solution.java
@@ -5,4 +5,8 @@ package lesson14.exercise01;
 Метод выводит все числа кратные 3 от нуля до какого-то числа (входной параметр)
  */
 public class Solution {
+    public static void main(String[] args) {
+
+    }
+
 }
