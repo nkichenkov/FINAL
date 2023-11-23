a172d6a7e86a000499a7ab48edeb40a3180df9d3
nkichenkov <151444715+nkichenkov@users.noreply.github.com>
2023-11-23 14:55:13 +0300

    .

--- src/lesson13/exercise01/Solution.java
+++ src/lesson13/exercise01/Solution.java
@@ -6,4 +6,33 @@ package lesson13.exercise01;
  */
 
 public class Solution {
+    public static void main(String[] args) {
+        print(minNumber(new int[]{1}));
+        print(minNumber(new int[]{2, 1, -1, 5, -3, 50}));
+    }
+
+    private static int minNumber(int[] numbers) {
+        int min;
+        if (numbers.length == 0) {
+            print("Empty array!");
+            min = 0;
+        } else {
+            min = numbers[0];
+
+            for (int i = 1; i < numbers.length; i++) {
+                if (numbers[i] < min) {
+                    min = numbers[i];
+                }
+            }
+        }
+        return min;
+    }
+
+    private static void print(int x) {
+        System.out.println(x);
+    }
+
+    private static void print(String s) {
+        System.out.println(s);
+    }
 }
