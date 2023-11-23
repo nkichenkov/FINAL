116e009e4347253db37b99795e58621195530cf6
nkichenkov <151444715+nkichenkov@users.noreply.github.com>
2023-11-23 17:50:41 +0300

    .

--- src/lesson15/exercise01/Solution.java
+++ src/lesson15/exercise01/Solution.java
@@ -1,5 +1,7 @@
 package lesson15.exercise01;
 
+import java.util.Scanner;
+
 /*
 Условие задачи:
 Написать программу, которая будет приветствовать всех, кроме Даниила. т.е. вы вводите имя, программ выдает
@@ -7,4 +9,15 @@ package lesson15.exercise01;
  */
 
 public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String helloText;
+        while (!(helloText = scanner.nextLine()).equals("Даниил")) {
+            print("Привет " + helloText);
+        }
+    }
+
+    private static void print(String s) {
+        System.out.println(s);
+    }
 }
--- src/lesson15/exercise02/Solution.java
+++ src/lesson15/exercise02/Solution.java
@@ -1,5 +1,7 @@
 package lesson15.exercise02;
 
+import java.util.Scanner;
+
 /*
 Условие задачи:
 Для каждого введенного в консоль числа проверять кратно ли оно 5 или нет. Если кратно, то выводить в консоль - кратное 5.
@@ -7,4 +9,24 @@ package lesson15.exercise02;
  */
 
 public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        int count = 0;
+        while (count < 20) {
+
+            number = scanner.nextInt();
+
+            if (number % 5 == 0) {
+                print(number + " кратно 5");
+            } else {
+                print(number + " не кратно 5");
+            }
+            count++;
+        }
+    }
+
+    private static void print(Object o) {
+        System.out.println(o);
+    }
 }
