cf320ded924711577b7469a5b63d8c6bdea2da9b
nkichenkov <151444715+nkichenkov@users.noreply.github.com>
2023-12-11 19:42:06 +0300

    .

--- src/lesson20/exercise01/Solution.java
+++ src/lesson20/exercise01/Solution.java
@@ -3,9 +3,16 @@ package lesson20.exercise01;
 /*
 Условие задачи:
 Напишите класс Прямоугольник у которого будет возможность породить объект с помощью точек. Пусть будет private метод
-на определение квадрат это или нет и public для описания объекта. Можете добавить еще private метод для расчета площади
-и использовать его в методе описания объекта
+на определение квадрат это или нет и public для описания объекта
  */
 
 public class Solution {
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(new Point(0, 0),
+                new Point(2, 0),
+                new Point(2, 3),
+                new Point(0, 3));
+        rectangle.isRightrectangle();
+    }
 }
